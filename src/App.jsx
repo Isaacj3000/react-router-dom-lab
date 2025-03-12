af33f5bbc5dfb29e381edb99c40cce877effd417
Isaacj3000 <Isaacjeanbaptiste95@gmail.com>
2025-03-12 00:22:51 -0400

    updated the import routes and added the links to new mailbox and mailboxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
 // src/App.jsx
+import NavBar from './components/NavBar/NavBar.jsx';
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // 
+import MailboxList from './components/MailboxList/MailboxList.jsx';
+import MailboxForm from './components/MailboxForm/MailboxForm.jsx';
+import MailboxDetails from './components/MailboxDetails/MailboxDetails.jsx';
+import * as React from 'react';
+import './App.css';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [mailboxes, setMailboxes] = useState([]);
+
+  const addBox = (newMailbox) => {
+    const newId = mailboxes.length ? mailboxes[mailboxes.length - 1]._id + 1 : 1;
+    const mailboxWithId = { 
+      ...newMailbox,
+       _id: newId +1 
+      };
+    setMailboxes((prevMailboxes) => [...prevMailboxes, mailboxWithId]);
+  };
+
+  return (
+    <Router> 
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <main>
+                <h1>Post Office</h1>
+                <p>
+                Welcome to the Post Office! Click on{' '}
+                <Link to="/new-mailbox">New MailBox</Link> to receive your mail or{' '}
+                <Link to="/mailboxes">check your mailbox</Link> to see everymail.
+              </p>
+              </main>
+            }
+          />
+          {/* Passing mailboxes as props to MailboxList */}
+          <Route path="/mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
+          <Route path="/new-mailbox" element={<MailboxForm onSubmit={addBox} />} />
+          <Route path="/mailboxes/:mailboxId" element={<MailboxDetails mailboxes={mailboxes} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
